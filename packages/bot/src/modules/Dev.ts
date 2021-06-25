9f4dd59f50d57d969218e902f7511666a307d752
Floffah <therealfloffah@gmail.com>
2021-06-25 08:38:53 +0100

    dont ping me bruv

--- packages/bot/src/modules/Dev.ts
+++ packages/bot/src/modules/Dev.ts
@@ -11,6 +11,20 @@ export default class Dev extends Module {
 
         this.bot.on("message", async (m) => {
             if (
+                m.guild &&
+                m.guild.id === "697340602504970261" &&
+                [
+                    "697340603066744864",
+                    "774699998985191494",
+                    "774615671488249908",
+                ].includes(m.channel.id)
+            ) {
+                m.reply(
+                    "Floffah is not available for support in this server. Please ping another member of support or member of staff to address your issue.",
+                );
+            }
+
+            if (
                 m.mentions.members &&
                 m.author.id === "221524691079266314" &&
                 m.mentions.members.size > 0
