eead6e8bd5f790f4ad7df428a68f0ecb72834f6f
Floffah <27270386+Floffah@users.noreply.github.com>
2021-08-22 19:43:30 +0100

    shard auto updating

--- packages/bot/src/util/shards.ts
+++ packages/bot/src/util/shards.ts
@@ -1,5 +1,6 @@
 import chalk from "chalk";
 import { ShardingManager } from "discord.js";
+import execa from "execa";
 import { existsSync, readFileSync } from "fs";
 import { parse } from "ini";
 import { resolve } from "path";
@@ -33,37 +34,41 @@ export async function startShards() {
         log(`Shard ${shard.id} created`);
     });
 
+    async function respawnShards() {
+        log(`Respawning all shards`);
+        const which: number[] = [];
+        const faults: number[] = [];
+
+        for (const s of shards.shards.values()) {
+            log(`Respawning shard ${s.id}`);
+            try {
+                await s.respawn();
+                which.push(s.id);
+            } catch (e) {
+                log(`Shard ${s.id} failed to respawn. ${e.message}`, true);
+                faults.push(s.id);
+            }
+        }
+
+        log(
+            `Respawned ${which.length} ${pluralize(
+                "shards",
+                which.length,
+            )} successfully (${[...which.join(",")]}) ${
+                faults.length > 0
+                    ? `with ${faults.length} broken ${pluralize(
+                          "shards",
+                          faults.length,
+                      )} (${faults.join(",")})`
+                    : ""
+            }`,
+        );
+    }
+
     keypress(process.stdin);
     process.stdin.on("keypress", async (_s, key) => {
         if (key.ctrl && key.name === "r") {
-            log(`Respawning all shards`);
-            const which: number[] = [];
-            const faults: number[] = [];
-
-            for (const s of shards.shards.values()) {
-                log(`Respawning shard ${s.id}`);
-                try {
-                    await s.respawn();
-                    which.push(s.id);
-                } catch (e) {
-                    log(`Shard ${s.id} failed to respawn. ${e.message}`, true);
-                    faults.push(s.id);
-                }
-            }
-
-            log(
-                `Respawned ${which.length} ${pluralize(
-                    "shards",
-                    which.length,
-                )} successfully (${[...which.join(",")]}) ${
-                    faults.length > 0
-                        ? `with ${faults.length} broken ${pluralize(
-                              "shards",
-                              faults.length,
-                          )} (${faults.join(",")})`
-                        : ""
-                }`,
-            );
+            await respawnShards();
         } else if (key.ctrl && key.name === "c") {
             for (const s of shards.shards.values()) {
                 s.kill();
@@ -85,4 +90,44 @@ export async function startShards() {
     await shards.spawn({ amount: "auto" });
 
     log(`Spawned shards. Press CTRL+R to respawn them`);
+
+    async function checkForShardUpdates() {
+        if (process.env.NODE_ENV === "development") return;
+
+        log("Checking for updates");
+
+        const execopts: execa.SyncOptions<string> = {
+            stdio: "pipe",
+            cwd: process.cwd(),
+            env: process.env,
+        };
+
+        const pull = execa.commandSync("git pull", execopts);
+
+        if (`${pull.stdout}`.toLowerCase().includes("already up to date")) {
+            log("No updates");
+            return;
+        }
+
+        log("Installing dependencies");
+        execa.commandSync("yarn", execopts);
+
+        log("Applying database migrations");
+        execa.commandSync("yarn prisma migrate deploy", execopts);
+
+        log("Generating database client");
+        execa.commandSync("yarn prisma generate", execopts);
+
+        log("Building code");
+        execa.commandSync("yarn workspace crossant tsup --minify", execopts);
+
+        log("Restarting shards");
+        await respawnShards();
+    }
+
+    await checkForShardUpdates();
+
+    setInterval(() => {
+        checkForShardUpdates();
+    }, 1000 * 60 * 5);
 }
